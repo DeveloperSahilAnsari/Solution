9b5ac839f9cc4c06bc65b2bb59b2f794438c4b8d
dac <dac@dypm.org>
2022-12-22 16:28:21 +0530

    arbaj shaikh

--- express_app/server.js
+++ express_app/server.js
@@ -9,14 +9,14 @@ app.get('/',(req,resp)=>{
 
 });
 
-
-
-
 app.get('/sahil',(req,resp)=>{
 resp.send("this is Sahil")
-
-
 });
+
+app.get('/arbaj',(req,resp)=>{
+    resp.send("this is arbaj shaikh")
+    });
+
 
 app.listen(3000);
 console.log("Server started at port no 3000");
